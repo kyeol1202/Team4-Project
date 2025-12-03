a1b23a6fffe681a685d29cc85cd790b4c3bec4cb
kyeol1202 <kyeol9927@gmail.com>
2025-12-03 14:11:55 +0900

    완료

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,26 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Main from "./component/main"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+  function App() {
   return (
+
+    
     <>
-  {/*
+    {/*
     네비게이터 연결부분
 
     Main = 메인화면
     */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />} />
+          </Routes>
+        </BrowserRouter>
     </>
+    
+  );
 
-  )
+  
 }
-
-export default App
+export default App;
