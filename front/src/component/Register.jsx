34300e6300cbd5499e863a9cc5a994abe029681e
sej040322-droid <sej040322@gmail.com>
2025-12-04 16:19:08 +0900

    회원가입

--- front/src/component/Register.jsx
+++ front/src/component/Register.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 
@@ -6,23 +6,29 @@ function Register(){
     const navigate = useNavigate();
     const [id,setId] = useState('');
     const [pw,setPw] = useState('');
+    const [pwCheck, setPwCheck] = useState('');
     const [name,setName] = useState('');
     const [email,setEmail] = useState('');
-    const [adress,setAdress] = useState('');
+    const [address,setAddress] = useState('');
     const [number,setNumber] = useState('');
     const [hbd,setHbd] = useState(''); //hbd >> 생년월일
 
     function register() {
-        if(!id || !pw) { //id와 pw가 비어있을 때
-        alert("아이디와 비밀번호를 입력하세요.");
+        if(!id || !pw || !name || !email || !address || !number || !hbd) { //필수항목이 비어있을 때
+        alert("필수항목을 입력해주세요");
         return;
         }
+        if (pw !== pwCheck) {
+        alert("비밀번호가 일치하지 않습니다");
+        return;
+        }
+
         //LocalStorage에 저장
         localStorage.setItem('id',id);
         localStorage.setItem('pw',pw);
         localStorage.setItem('name',name);
         localStorage.setItem('email',email);
-        localStorage.setItem('adress',adress);
+        localStorage.setItem('address',address);
         localStorage.setItem('number',number);
         localStorage.setItem('hbd',hbd);
 
@@ -44,7 +50,11 @@ function Register(){
             </div>
             <div>
             <div>비밀번호</div>
-            <input type="text" value={pw} onChange={(e)=>setPw(e.target.value)}/>
+            <input type="password" value={pw} onChange={(e)=>setPw(e.target.value)}/>
+            </div>
+            <div>
+            <div>비밀번호 확인</div>
+            <input type="password" value={pwCheck} onChange={(e)=>setPwCheck(e.target.value)} />
             </div>
             <div>
             <div>성함</div>
@@ -56,20 +66,18 @@ function Register(){
             </div>
             <div>
             <div>주소</div>
-            <input type="text" value={adress} onChange={(e)=>setAdress(e.target.value)}/>
+            <input type="text" value={address} onChange={(e)=>setAddress(e.target.value)}/>
             </div>
             <div>
             <div>전화번호</div>
             <input type="text" value={number} onChange={(e)=>setNumber(e.target.value)}/>
             </div>
             <div>
-            <div>아이디</div>
+            <div>생년월일</div>
             <input type="text" value={hbd} onChange={(e)=>setHbd(e.target.value)}/>
             </div>
             <button onClick={() => navigate("/")}>🏡</button>
-            <button onClick={register}>
-                회원가입
-            </button>
+            <button onClick={register}>회원가입</button>
         </>
     )
 
