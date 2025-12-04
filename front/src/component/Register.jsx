52199e6643c41f482bf7c59224c3aed69c273a66
sej040322-droid <sej040322@gmail.com>
2025-12-04 15:22:27 +0900

    회원가입틀

--- front/src/component/Register.jsx
+++ front/src/component/Register.jsx
@@ -4,11 +4,36 @@ import { useNavigate } from "react-router-dom";
 
 function Register(){
     const navigate = useNavigate();
+    const [id,setId] = useState('');
+    const [pw,setPw] = useState('');
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [adress,setAdress] = useState('');
+    const [number,setNumber] = useState('');
+    const [hbd,setHbd] = useState(''); //hbd >> 생년월일
 
+    function Register() {
+        localStorage.setItem('id',id);
+        localStorage.setItem('pw',pw);
+        localStorage.setItem('name',name);
+        localStorage.setItem('email',email);
+        localStorage.setItem('adress',adress);
+        localStorage.setItem('number',number);
+        localStorage.setItem('hbd',hbd);
+        
+        
+    }
+    
     return(
 
         <>
-            회원가입페이지
+            <input onChange={(e)=>setId(e.target.value)}/>
+            <input onChange={(e)=>setPw(e.target.value)}/> 
+            <input onChange={(e)=>setName(e.target.value)}/>
+            <input onChange={(e)=>setEmail(e.target.value)}/>  
+            <input onChange={(e)=>setAdress(e.target.value)}/>
+            <input onChange={(e)=>setNumber(e.target.value)}/>  
+            <input onChange={(e)=>setHbd(e.target.value)}/> 
             <button onClick={() => navigate("/")}>이전</button>
         </>
     )
