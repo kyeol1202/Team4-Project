49b2c206272b95a9b67bff6f9b5886588e3241a6
“fainjun014-create <fainjun014@gmail.com>
2025-12-05 12:01:32 +0900

    수정

--- front/src/component/Category.jsx
+++ front/src/component/Category.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useCart } from "../hooks/useCart";
+import { useCart } from "../context/CartContext";
 import { useWish } from "../context/WishContext";
 
 
