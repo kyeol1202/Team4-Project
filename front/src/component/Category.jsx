d1e895b7aad4aaab268ae3f17fdfd6bb71db3012
“fainjun014-create <fainjun014@gmail.com>
2025-12-05 11:59:52 +0900

    수정

--- front/src/component/Category.jsx
+++ front/src/component/Category.jsx
@@ -1,14 +1,21 @@
 import { useNavigate } from "react-router-dom";
+import { useCart } from "../hooks/useCart";
+import { useWish } from "../context/WishContext";
+
 
 function Category() {
     const navigate = useNavigate();
     const { addToCart } = useCart();
+    const { addToWish } = useWish();
+
+    const p = { id: 1, name: "상품1", price: 1000};
 
     return (
 
         <>
             카테고리1페이지
             <button onClick={() => navigate("/")}>이전</button>
+            <button onClick={() => addToWish(p)}>찜하기</button>
             <button onClick={() => addToCart(p)}>장바구니 담기</button>
         </>
     )
--- front/src/component/main.jsx
+++ front/src/component/main.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+
+
+
 function Main() {
     const [loginOpen, setLoginOpen] = useState(false);
     const [login, setLogin] = useState(false);
