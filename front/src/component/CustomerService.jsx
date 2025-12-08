de0392a3a30b733f862b94cf16d50454dbce1cfe
fainjun014-create <fainjun014@gmail.com>
2025-12-08 12:10:39 +0900

    aa

--- front/src/component/CustomerService.jsx
+++ front/src/component/CustomerService.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
-import {ChevronDown, ChevronUp} from "react-feather";
+import { ChevronDown, ChevronUp } from "react-feather";
 
 function CustomerService() {
     const navigate = useNavigate();
@@ -26,7 +26,7 @@ window.open(url, "_blank");
 }
 };
     return (
-        <div className="bg-white text-black min-h-screen px-8 py-14 max-w-4xl mx-auto font-[Cormorant]]">
+        <div className="bg-white text-black min-h-screen px-8 py-14 max-w-4xl mx-auto font-[Cormorant]">
            <h1 className="text-4xl font-bold text-center mb-14 tracking-wider">고객센터</h1> 
            
            {/*공지사항*/}
