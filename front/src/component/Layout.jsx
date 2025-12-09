3374d00db16283954903e8459c563fa107cadd14
bellkong7079 <jongbin344@gmail.com>
2025-12-09 15:34:05 +0900

    123

--- front/src/component/Layout.jsx
+++ front/src/component/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet, useNavigate } from "react-router-dom";
-import { useState} from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useCart } from "../context/CartContext";
 import { useWish } from "../context/WishContext";
