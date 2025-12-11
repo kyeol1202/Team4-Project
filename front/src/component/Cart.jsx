ddc760b799de88599652f1a12e7f6a9595fcd236
fainjun014-create <fainjun014@gmail.com>
2025-12-11 15:03:09 +0900

    ㅁ

--- front/src/component/Cart.jsx
+++ front/src/component/Cart.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = "http://http://192.168.0.224";
+const API_URL = "http://http://192.168.0.224:8080";
 
 function Cart() {
   const navigate = useNavigate();
